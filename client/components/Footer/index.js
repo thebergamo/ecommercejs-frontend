740bb168652ab251948e72fd818a5a7d9bbac209
Henrique Sosa <henriquesosa@gmail.com>
2016-01-11 15:39:33 -0200

    Start building grid system

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -6,13 +6,21 @@ class Footer extends Component {
 	render() {
 		return(
 			<footer>
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-3">store info</div>
-						<div className="col-lg-2">links 1</div>
-						<div className="col-lg-2">links 2</div>
-						<div className="col-lg-2">links 3</div>
-						<div className="col-lg-3">newsletter</div>
+				<div className="footer">
+					<div className="container">
+						<div className="row">
+							<div className="col-lg-3">store info</div>
+							<div className="col-lg-2">links 1</div>
+							<div className="col-lg-2">links 2</div>
+							<div className="col-lg-2">links 3</div>
+							<div className="col-lg-3">newsletter</div>
+						</div>
+					</div>
+				</div>
+				<div className="footer-bottom">
+					<div className="container">
+						<p className="pull-left"> © Ecommerce.js 2016. All right reserved.</p>
+						<p className="pull-right">cards</p>
 					</div>
 				</div>
 			</footer>
